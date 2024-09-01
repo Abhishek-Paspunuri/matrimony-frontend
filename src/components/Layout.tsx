162eff7c95e04065656a43874b12005a0ad32a1d
Abhishek Paspunuri <abhishek.paspunuri@gmail.com>
2024-09-01 15:24:56 +0530

    Fix Responsiveness for Mobile

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,9 +2,13 @@ import Home from './Home'
 const Layout = () => {
     return (
         <div>
-            <div className='w-full h-[25vh] bg-purple-300'></div>
+            <div className='w-full h-[25vh] bg-purple-300'>
+                <p className='text-2xl text-purple-900 font-bold pt-4 pl-5 sm:pl-[9rem] md:pl-[16rem] italic'>
+                    My Matrimony
+                </p>
+            </div>
             <div className='w-full bg-gray-100'></div>
-            <div className='w-full px-12 absolute top-[100px]'>
+            <div className='w-full px-3 md:px-12 absolute top-20'>
                 <Home />
             </div>
         </div>
