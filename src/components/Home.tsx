8ac113397c6a9012633537cf67f1701dabe3e502
Abhishek Paspunuri <abhishek.paspunuri@gmail.com>
2024-09-01 16:16:07 +0530

    Removed unused dependencies

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useMemo, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import Corousel from './Corousel'
 import PersonalDetails from './PersonalDetails'
 import FooterButtons from './FooterButtons'
